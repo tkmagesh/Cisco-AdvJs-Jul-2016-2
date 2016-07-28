1aeb1376d6de45151cfa9263c45c5cea392dad47
Magesh <tkmagesh77@gmail.com>
2016-07-28 17:00:26 +0530

    Day-01 Complete

---
+++ Day-01/02-Closures/isPrime.js
@@ -0,0 +1,15 @@
+function isPrime(x){
+	
+}
+
+isPrime(100); // runs the algo, returns false
+isPrime(101); // runs the algo, returns true
+isPrime(102); // runs the algo, returns false
+isPrime(103); // runs the algo, returns true
+
+isPrime(100); // DO NOT run the algo, returns false
+isPrime(101); // DO NOT run the algo, returns true
+isPrime(102); // DO NOT run the algo, returns false
+isPrime(103); // DO NOT run the algo, returns true
+
+isPrime(104); // runs the algo, returns false
